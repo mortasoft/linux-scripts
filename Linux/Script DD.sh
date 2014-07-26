3faac9acc031640120ec1341a6045f1eb574f108
mortasoft <mortasoft@gmail.com>
2014-07-26 13:21:13 -0600

    Mas Cambios

--- Linux/Script DD.sh
+++
@@ -1,5 +0,0 @@
-dd if=/home/mortasoft/imagen.dd of=/dev/sde conv=notrunc & dd if=/home/mortasoft/imagen.dd of=/dev/sdf conv=notrunc & dd if=/home/mortasoft/imagen.dd of=/dev/sdg conv=notrunc & dd if=/home/mortasoft/imagen.dd of=/dev/sdh conv=notrunc
-
-
-
-
