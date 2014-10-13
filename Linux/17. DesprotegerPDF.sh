6ca8887244095451d2ee9eacc53c947fdebb0a12
Mario Zamora <mortasoft@gmail.com>
2014-10-13 14:29:15 -0600

    Nuevos archivos

---
+++ Linux/17. DesprotegerPDF.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+qpdf --decrypt protected.pdf unprotected.pdf
+
+# Si el archivo PDF está protegido con un password, habrá que especificarlo con el parámetro --password, o utilizar # algún comando como pdfcrack para intentar reventarlo.
+
+
+# smtp.
+# aspmx.l.google.com : 25
---
+++ Linux/18. ModificarMasivo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i 's/paco/Francisco/g' *.txt
