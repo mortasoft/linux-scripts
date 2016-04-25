747a2267e850428a53a9ff65681647090865c1f9
Mario Z <mortasoft@gmail.com>
2016-04-24 22:34:39 -0600

    Cambios al script GNOME

--- Gnome/DespuesdeInstalar.sh
+++ Gnome/DespuesdeInstalar.sh
@@ -1,15 +1,15 @@
-##1. Directorios Primero:
+1. Directorios Primero:
 
-dconf-editor
-Navigate to the org.gnome.nautilus.preferences k
-sort-directories-first to true.
+# dconf-editor
+# Navigate to the org.gnome.nautilus.preferences k
+# sort-directories-first to true.
 
-Tambien en el File Chooser
+# Tambien en el File Chooser
 
-org.gtk.Settings.FileChooser 
-sort-directories-first to true.
+# org.gtk.Settings.FileChooser 
+# sort-directories-first to true.
 
-## 2. Top Icons
+2. Top Icons
 
-https://extensions.gnome.org/extension/495/topicons/
-Establish in ON (Top left)
+# https://extensions.gnome.org/extension/495/topicons/
+# Establish in ON (Top left)
