acff766d39f75882650d00e5ceb6250cdef7c3a1
Mario Zamora <mortasoft@gmail.com>
2021-10-16 21:50:28 -0600

    Mejoras

---
+++ Instalaciones/Por_instalar.sh
@@ -0,0 +1,17 @@
+VirtualBox
+Opera
+Chrome
+Firefox
+Visual Studio Code
+Zoom
+--Steam
+Calibre
+Mega Downloadeer
+WPS Telegram
+KDE Connect
+Any Desk
+Git Kraken
+Firma Digital???
+Stepmania!
+ObsStudio
+Obs Virtual Cam
