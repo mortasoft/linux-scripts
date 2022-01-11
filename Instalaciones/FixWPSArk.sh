7ee2d3e344578ebdfb3ec89ee045f106ef4420c9
Mario <mortasoft@gmail.com>
2022-01-10 21:32:49 -0600

    Manjaro Update

--- Instalaciones/FixWPSArk.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-#
-# Script para solucionar problema con WPS en Manjaro
-#
-cd /usr/share/mime/packages/
-sudo rm wps-office-et.xml 
-sudo rm wps-office-wpp.xml
-sudo rm wps-office-wps.xml
-sudo update-mime-database /usr/share/mime
--- Instalaciones/java.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Para instalar JAVA 8 desde PPA
-#
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
